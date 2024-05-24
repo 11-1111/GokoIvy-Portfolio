f5e4c258f9556545e6ca6a19419a028a6d760af7
11-1111 <153610684+11-1111@users.noreply.github.com>
2024-05-24 14:12:01 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+  const menu = document.getElementById('menu');
+  if (menu.style.display === 'block') {
+    menu.style.display = 'none';
+  } else {
+    menu.style.display = 'block';
+  }
 }
+
 
 let sections = document.querySelectorAll("section");
 
